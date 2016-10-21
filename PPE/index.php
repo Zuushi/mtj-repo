6846b0ed2fe28303dcc287381fdedbb2b5187871
Zuushi <jonathan.graille@gmail.com>
2016-10-21 11:13:14 +0200

    Maj index.php
    ajout du slider + connexion amélioré

--- PPE/index.php
+++ PPE/index.php
@@ -19,11 +19,14 @@ include_once('includes/co_bdd.php');
     <!-- Bootstrap core CSS -->
     <link href="../PPE/dist/css/bootstrap.css" rel="stylesheet">
 
+    <!-- Custom CSS -->
+    <link href="css/full-slider.css" rel="stylesheet">
+
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="../PPE/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="PPE/jumbotron.css" rel="stylesheet">
+    <link href="jumbotron.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -47,30 +50,80 @@ include_once('includes/co_bdd.php');
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a href="index.php"><img style="" src="img/Sphere5.png"></a>
+          <a href="index.php"><img style="" src="img/Sphere4.png"></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <form class="navbar-form navbar-right">
-            <div class="form-group">
-              <input type="text" placeholder="Email" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Mot de passe" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-primary">Connexion</button>
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#connexion">Connexion</button>
             <a href="inscription.php"><button type="button" class="btn btn-primary">Inscription</button></a>
           </form>
         </div><!--/.navbar-collapse -->
       </div>
     </nav>
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container">
-        <h1>Hello, world!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+<!-- Connexion -->
+<div class="modal fade" id="connexion" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h3 class="col-md-offset-4">Connexion...</h3>
       </div>
+      <!-- Contenu de l'inscription Freelance-->
+      <div class="modal-body">
+        <div class="row">
+            <div class="col-md-offset-2 col-md-7">
+                <div class="form-group">
+                    <label for="Email">Adresse Mail</label>
+                    <input type="text" class="form-control" name="email" placeholder="Entrez l'email">
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-offset-2 col-md-7">
+                <div class="form-group">
+                    <label for="Password">Mot de passe</label>
+                    <input type="password" class="form-control" name="password" placeholder="Mot de passe">
+                </div>
+            </div>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Retour</button>
+        <button type="submit" class="btn btn-primary">Connexion</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <!-- Full Page Image Background Carousel Header -->
+    <div class="full_slide">
+    <header id="myCarousel" class="carousel slide">
+        <!-- Wrapper for Slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <!-- Set the first background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('img/reseau.jpg');"></div>
+            </div>
+            <div class="item">
+                <!-- Set the second background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('img/couverture.jpg');"></div>
+            </div>
+<!--        <div class="item">
+                 Set the third background image using inline CSS below.
+                <div class="fill" style="background-image:url('img/couverture.jpg');"></div>
+            </div> -->
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+
+    </header>
     </div>
 
     <div class="container">
@@ -104,10 +157,17 @@ include_once('includes/co_bdd.php');
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="../PPE/dist/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+         <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })  
+    </script>
     <script src="../PPE/assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
