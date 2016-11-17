1f7a6cfd44fb8313ed34de43b8e625d77d11fe0b
Zuushi <jonathan.graille@gmail.com>
2016-11-17 16:30:26 +0100

    nouvel index

--- PPE/index.php
+++ PPE/index.php
@@ -13,25 +13,22 @@ include_once('includes/traitement_co_free.php');
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
   <meta name="description" content="">
   <meta name="author" content="">
-  <link rel="icon" href="../../favicon.ico">
+  <link rel="icon" href="../favicon.ico">
 
   <title>PPE Site Freelance</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="../PPE/dist/css/bootstrap.css" rel="stylesheet">
+  <link href="dist/css/bootstrap.css" rel="stylesheet">
 
   <!-- Custom CSS -->
   <link href="css/full-slider.css" rel="stylesheet">
 
   <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-  <link href="../PPE/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="jumbotron.css" rel="stylesheet">
+  <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
   <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
   <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-  <script src="../PPE/assets/js/ie-emulation-modes-warning.js"></script>
+  <script src="assets/js/ie-emulation-modes-warning.js"></script>
 
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -81,23 +78,18 @@ include_once('includes/traitement_co_free.php');
          else
          {
           ?>
-                <nav class="navbar navbar-inverse">
-        <div class="container">
+        <nav class="navbar navbar-inverse">
+         <div class="principal-top">
           <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
             <a href="index.php"><img class="img-responsive" style="" src="img/Sphere4.png"></a>
           </div>
-          <div id="navbar" class="navbar-collapse collapse">
-            <form class="navbar-form navbar-right">
+          <input type="search" class="input-xl form-control bar-form" placeholder="Mot-clés..."><button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span> Chercher</button>
+          <a class="lien-nav" href=""><B>TROUVER UN FREELANCE</B></a>
+          <a class="lien-nav" href=""><B>TROUVER UNE MISSION</B></a>
+            <form class="barre-de-recherche">
               <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#connexion">Connexion</button>
-              <a href="inscription.php"><button type="button" class="btn btn-primary">Inscription</button></a>
+              <a href="inscription.php"><button type="button" class="btn btn-primary">Créer un compte</button></a>
             </form> 
-
           </div><!--/.navbar-collapse -->
         </div>
       </nav>
@@ -161,14 +153,14 @@ include_once('includes/traitement_co_free.php');
           <div class="carousel-inner">
             <div class="item active">
               <!-- Set the first background image using inline CSS below. -->
-              <div class="fill" style="background-image:url('img/reseau.jpg');"></div>
+              <div class="fill" style="background-image:url('img/bandeau1.jpg');"></div>
             </div>
             <div class="item">
               <!-- Set the second background image using inline CSS below. -->
-              <div class="fill" style="background-image:url('img/focus.jpg');"></div>
-            </div>
+              <div class="fill" style="background-image:url('img/bandeau2.png');"></div>
+            </div>       
             <div class="item">
-              <div class="fill" style="background-image:url('img/webd.jpg');"></div>
+              <div class="fill" style="background-image:url('img/bandeau4.jpg');"></div>
             </div>
           </div>
 
@@ -183,31 +175,143 @@ include_once('includes/traitement_co_free.php');
         </header>
       </div>
 
-      <div class="container">
+      <div>
         <!-- Example row of columns -->
-        <div class="row">
-          <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-          </div>
-          <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-          </div>
-          <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-          </div>
-        </div>
-
+<div class="mid-principal">
+    <div class="container">
+    </div>
+    <div>
+    <span class="center-title"><h2>Les étapes pour un départ réussi:
+    <button class="btn-choix">JE SUIS UN FREELANCE</button><button class="btn-choix">JE SUIS UN FREELANCE</button></h2></span>
+    </div><br>
+</div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
         <hr>
-
-        <footer>
-          <p>&copy; 2016 Company, Inc.</p>
-        </footer>
+<div class="mid-principal">
+    <div class="container">
+<div class="clear"></div>
+    </div>
+    <div>
+    <br><br>
+    <center><h1>Nos partenaires : &nbsp;
+    	<img class="part" src="img/logo_part/air-logo.png">&nbsp;&nbsp;
+    	<img class="part2" src="img/logo_part/logo-etat.png">&nbsp;&nbsp;
+    	<img class="part2" src="img/logo_part/sophia-logo1.png">&nbsp;&nbsp;
+    	<img class="part2" src="img/logo_part/eurocop.png">&nbsp;&nbsp;
+    </h1></center>
+        <br clear="all"><br><br>
+    </div>
+</div>
+<div class="footer-principal">
+    <div class="container">
+<div class="clear"></div>
+    </div>
+        <div class="container">
+            <div class="col-lg-12">
+                <br />
+                <a href="https://www.facebook.com"><img class="logo-icon" src="img/fb.png"></a>
+                <a href="https://https://twitter.com/?lang=fr"><img class="logo-icon" src="img/Twitter.png"></a>
+                <a href="https://getbootstrap.com"><img class="logo-icon" src="img/Boostrap.png"></a>
+                <a href="https://www.youtube.fr"><img class="logo-icon" src="img/Youtube.png"></a>
+                <a href="https://www.google.fr"><img class="logo-icon2" src="img/google.png"></a>
+                <span style="position:relative;top:6px;left:5%;font-size:30px;">Suivez-nous sur les différentes plateformes!</span>
+            </div>
+        </div>
+    <hr class="hrwhite">
+    <div class="container">
+        <div class="homemenu">
+            <div class="col-md-4 col-lg-4 footerblokk">
+                <div class="tittle">Services aux sociétés:</div>
+                <div><a href="/fr/corp">- freelance.com/CORP</a></div>
+                <div><a href="/fr/direct">- freelance.com/DIRECT</a></div>
+                <div><a href="/fr/interco">- freelance.com/INTERCO</a></div>
+            </div>
+            <div class="col-md-4 col-lg-4 footerblokk">
+                <div class="tittle">Services aux freelances:</div>
+                <div><a href="/fr/contents/vigielance">- Provigis</a></div>
+                <div><a href="/fr/contents/valor-portage-com">- Portage Salarial</a></div>
+            </div>
+            <div class="col-md-4 col-lg-4 footerblokk">
+                <div class="tittle">FAQ:</div>
+                <div>
+                    
+                    
+                        
+                    
+                    <a href="http://freelance-sphere.com/?">- FAQ pour les sociétés</a>
+                </div>
+                <div>
+                    
+                    
+                        
+                    
+                    <a href="http://support.freelance.com/hc/fr/sections/200513328-Documentation-freelance">- FAQ pour les freelances</a>
+                </div>
+            </div>
+            <br clear="all"><br><br>
+            <div class="col-md-4 col-lg-4 footerblokk">
+                <div class="tittle">Contactez-nous:</div>
+                
+                
+                    
+                
+                <div><a href="/fr/contents/nous-contacter#headquarters">- Siège social</a></div>
+                <div>
+                    
+                    
+                        
+                    
+                    <a href="http://support.freelance.com/hc/fr/requests/new">- Un problème technique</a>
+                </div>
+                <div><a href="/fr/contents/nous-contacter#workWithUs">- Travailler chez freelance-sphere.com</a></div>
+            </div>
+            <div class="col-md-4 col-lg-4 footerblokk">
+                <div class="tittle">Investisseurs:<a href="mailto:investisseurs@freelance.com" class="expose"> investisseurs@freelance-sphere.com</a></div>
+                <div>
+                    
+                    
+                        
+                    
+                    <a href="/fr/contents/investisseurs">- Quelques chiffres clés</a>
+                </div>
+                <div>
+                    <a href="/fr/contents/informations-reglementees">- Informations réglementées</a>
+                </div>
+                <div>
+                    <a href="/fr/contents/espace-actionnaires">- Espace actionnaires</a>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 footerblokk">
+                <div class="tittle">Qui sommes-nous?:</div>
+                <div>
+                    
+                    
+                        
+                    
+                    <a href="/fr/contents/a-propos-de-freelance-com">- A propos de freelance-sphere.com</a>
+                </div>
+                
+         
+                <div>
+                    
+                    
+                        
+                    
+                    <a href="/fr/contents/l-equipe-de-direction">- Notre équipe de professionnels</a>
+                </div>
+                <div>
+                    
+                    
+                        
+                    
+                    <a href="/fr/contents/temoignages">- Témoignages</a>
+                </div>
+                <div><a href="/fr/contents/terms">- Mentions légales</a></div>
+            </div>
+        </div>
+        <br clear="all"><br><br>
+    </div>
+</div>
       </div> <!-- /container -->
 
 
@@ -225,6 +329,6 @@ include_once('includes/traitement_co_free.php');
         interval: 5000 //changes the speed
       })  
     </script>
-    <script src="../PPE/assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
   </html>
