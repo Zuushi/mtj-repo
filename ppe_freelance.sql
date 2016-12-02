7494d4697d65d45fb9f554a6e08a7613cbaf6a8f
Zuushi <jonathan.graille@gmail.com>
2016-12-02 09:30:58 +0100

    ajout de la table qcm dans la base

--- ppe_freelance.sql
+++ ppe_freelance.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.5
+-- version 4.5.4.1deb2ubuntu2
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Generation Time: Dec 01, 2016 at 07:55 AM
--- Server version: 5.5.49-log
--- PHP Version: 7.0.9
+-- Client :  localhost
+-- Généré le :  Ven 02 Décembre 2016 à 09:27
+-- Version du serveur :  5.7.16-0ubuntu0.16.04.1
+-- Version de PHP :  7.0.8-0ubuntu0.16.04.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,16 +17,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ppe_freelance`
+-- Base de données :  `ppe_freelance`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `annonces`
+-- Structure de la table `annonces`
 --
 
-CREATE TABLE IF NOT EXISTS `annonces` (
+CREATE TABLE `annonces` (
   `id_ann` int(11) NOT NULL,
   `titre` varchar(150) NOT NULL,
   `date_publi` date NOT NULL,
@@ -38,25 +38,25 @@ CREATE TABLE IF NOT EXISTS `annonces` (
   `competences` text NOT NULL,
   `cat` varchar(50) NOT NULL,
   `spe` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `annonces`
+-- Contenu de la table `annonces`
 --
 
 INSERT INTO `annonces` (`id_ann`, `titre`, `date_publi`, `date_debut`, `duree`, `salaire`, `description`, `lieu`, `competences`, `cat`, `spe`) VALUES
-(1, 'Site de vente', '2016-10-18', '2016-11-21', '1 mois', 5000, 'Création d''un site de vente en ligne.', 'Paris', 'Nous recherchons un développeur Symfony expérimenté. ', 'dev', 'php'),
-(2, 'Site vitrine', '2016-11-17', '2016-11-24', '1 semaine', 500, 'Création d''un site vitrine', 'Nice', 'Maîtrise du framework Bootstrap.', 'dev', 'css'),
-(3, 'Programme JAVA', '2016-11-25', '2016-12-23', '', 3000, 'Developpement d''un programme sous JAVA', 'Paris ', 'Maîtrise de JAVA', 'prog', 'java'),
+(1, 'Site de vente', '2016-10-18', '2016-11-21', '1 mois', 5000, 'Création d\'un site de vente en ligne.', 'Paris', 'Nous recherchons un développeur Symfony expérimenté. ', 'dev', 'php'),
+(2, 'Site vitrine', '2016-11-17', '2016-11-24', '1 semaine', 500, 'Création d\'un site vitrine', 'Nice', 'Maîtrise du framework Bootstrap.', 'dev', 'css'),
+(3, 'Programme JAVA', '2016-11-25', '2016-12-23', '', 3000, 'Developpement d\'un programme sous JAVA', 'Paris ', 'Maîtrise de JAVA', 'prog', 'java'),
 (4, 'Administrateur CISCO', '2016-11-25', '2016-11-29', '', 1300, 'Administration réseau', 'Marseille', 'Avoir la certification CISCO systeme', 'reseau', 'cisco');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mbr_admin`
+-- Structure de la table `mbr_admin`
 --
 
-CREATE TABLE IF NOT EXISTS `mbr_admin` (
+CREATE TABLE `mbr_admin` (
   `id_admin` int(11) NOT NULL,
   `pseudo` varchar(25) NOT NULL,
   `password` varchar(25) NOT NULL
@@ -65,10 +65,10 @@ CREATE TABLE IF NOT EXISTS `mbr_admin` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mbr_free`
+-- Structure de la table `mbr_free`
 --
 
-CREATE TABLE IF NOT EXISTS `mbr_free` (
+CREATE TABLE `mbr_free` (
   `id_free` int(11) NOT NULL,
   `nom` varchar(25) NOT NULL,
   `prenom` varchar(25) NOT NULL,
@@ -82,10 +82,10 @@ CREATE TABLE IF NOT EXISTS `mbr_free` (
   `tarif` int(11) NOT NULL,
   `langues` varchar(25) NOT NULL,
   `localisation` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `mbr_free`
+-- Contenu de la table `mbr_free`
 --
 
 INSERT INTO `mbr_free` (`id_free`, `nom`, `prenom`, `mail`, `password`, `date_inscr`, `test`, `competences`, `site_web`, `photo`, `tarif`, `langues`, `localisation`) VALUES
@@ -96,10 +96,10 @@ INSERT INTO `mbr_free` (`id_free`, `nom`, `prenom`, `mail`, `password`, `date_in
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mbr_society`
+-- Structure de la table `mbr_society`
 --
 
-CREATE TABLE IF NOT EXISTS `mbr_society` (
+CREATE TABLE `mbr_society` (
   `id_soc` int(11) NOT NULL,
   `raison_sociale` varchar(50) NOT NULL,
   `siret` int(11) NOT NULL,
@@ -115,45 +115,82 @@ CREATE TABLE IF NOT EXISTS `mbr_society` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `test`
+-- Structure de la table `qcm`
 --
 
-CREATE TABLE IF NOT EXISTS `test` (
+CREATE TABLE `qcm` (
+  `id_question` int(255) NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `reponse_v` varchar(255) NOT NULL,
+  `reponse_f1` varchar(255) NOT NULL,
+  `reponse_f2` varchar(255) NOT NULL,
+  `reponse_f3` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `qcm`
+--
+
+INSERT INTO `qcm` (`id_question`, `question`, `reponse_v`, `reponse_f1`, `reponse_f2`, `reponse_f3`) VALUES
+(1, 'Java et JavaScript sont-ils deux mêmes langages informatiques ?', 'Non, le Java est un langage orienté objet et compilé alors que le JavaScript est un langage de script et procédural.', 'Aucune', 'Non, le JavaScript est un langage orienté objet et compilé alors que le Java est un langage de script et procédural.', 'Oui, ce sont des langages orientés objet et compilés.'),
+(2, 'Quelle est la différence entre un bit et un byte ?', '8 bits = 1 octect, 1 byte = 1 octect.', 'Aucune.', '8 bits = 8 octect, 1 byte = 8 octect.', '1 bits = 1 octect, 1 byte = 1 octect.'),
+(3, 'Qu\'est-ce qu\'un switch ?', 'Un switch désigne un commutateur réseau, équipement ou appareil qui permet l\'interconnexion d\'appareils communicants, terminaux, ordinateurs, serveurs...', 'C\'est un ordinateur très puissant qui a été inventé par les Soviétiques dans les années 1980.', 'C\'est un appareil qui permet de scanner les codes-barres et de retransmettre les informations sur ordinateur.', 'C\'est un appareil qui permet de changer le sens de circulation du courant dans les cartes-mères.'),
+(4, 'Quelle est la fonction qui permet d\'afficher un message à l\'écran en Python ?', '"print".', '"printf".', '"scanf".', '"alert".'),
+(5, 'Quelle est la différence entre la base 10 et le système décimal ?', 'Aucune.', 'En base de 10 on compte de 10 en 10 alors qu\'en décimal on compte 1 en 1 (ex: 10, 20, 30.. 1, 2 ,3 ..).', 'La base de 10 est un langage orienté objet alors que le système décimal est un langage procédural.', 'En base de 10 on compte de 1 en 1 de même pour le système décimal (ex: 1, 2 ,3 ..).'),
+(6, 'Qu\'est-ce que le "Spanning Tree Protocol" ?', 'Le Spanning Tree Protocol est un protocole réseau de niveau 2 permettant de déterminer une topologie réseau sans boucle (appelée arbre) dans les LAN avec ponts.', 'Le Spanning Tree Protocol est un protocole réseau de niveau 3 permettant la connexion et déconnexion automatique de chaque ordinateur entrant et sortant sur un réseau local.', 'Le Spanning Tree Protocol est un protocole réseau en arbre qui détermine les privilèges de chaque branche d\'un réseau.', 'Le Spanning Tree Protocol est un système d\'exploitation Linux qui permet le débogage rapide de logiciels.'),
+(7, 'Qu\'est-ce qu\'une adresse MAC?', 'Une adresse MAC (Media Access Control), est un identifiant physique stocké dans une carte réseau ou une interface réseau similaire. Elle est réputé unique au monde.', 'Une adresse MAC (Mega Adress Card), ou IP, est associé à chaque carte réseau et logiciel de routage.', 'Une adresse MAC est une adresse de messagerie avec laquelle on peut contacter les administrateurs de forum.', 'L\'adresse MAC est une adresse à laquelle on peut accéder à tout les fichiers stockés sur un réseau.'),
+(8, 'En quelle année le langage C a-t-il était inventé ?', '1972', '1965', '1975', '1980'),
+(9, 'À partir de quelle version la notion de PHP Data Objects a-t-elle été instauré ?', '5.0', '6.0', '5.5', '7.0'),
+(10, 'Quel est le principe de git ?', 'git est un logiciel de gestion de versions décentralisé. C\'est un logiciel libre.', 'C\'est un logiciel de retouche d\'image et photo, comme Gimp par exemple.', 'C\'est un logiciel de capture d\'écran ainsi que de capture vidéo compatible sur tout les systèmes d\'exploitation.', 'git est un mini système d\'exploitation qu\'on peut intégrer à un Raspberry Pi 3.');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `test`
+--
+
+CREATE TABLE `test` (
   `id_test` int(11) NOT NULL,
   `result` varchar(25) NOT NULL,
   `id_free` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Indexes for dumped tables
+-- Index pour les tables exportées
 --
 
 --
--- Indexes for table `annonces`
+-- Index pour la table `annonces`
 --
 ALTER TABLE `annonces`
   ADD PRIMARY KEY (`id_ann`);
 
 --
--- Indexes for table `mbr_admin`
+-- Index pour la table `mbr_admin`
 --
 ALTER TABLE `mbr_admin`
   ADD PRIMARY KEY (`id_admin`);
 
 --
--- Indexes for table `mbr_free`
+-- Index pour la table `mbr_free`
 --
 ALTER TABLE `mbr_free`
   ADD PRIMARY KEY (`id_free`);
 
 --
--- Indexes for table `mbr_society`
+-- Index pour la table `mbr_society`
 --
 ALTER TABLE `mbr_society`
   ADD PRIMARY KEY (`id_soc`);
 
 --
--- Indexes for table `test`
+-- Index pour la table `qcm`
+--
+ALTER TABLE `qcm`
+  ADD PRIMARY KEY (`id_question`);
+
+--
+-- Index pour la table `test`
 --
 ALTER TABLE `test`
   ADD PRIMARY KEY (`id_test`),
@@ -161,40 +198,40 @@ ALTER TABLE `test`
   ADD KEY `id_free_2` (`id_free`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT for table `annonces`
+-- AUTO_INCREMENT pour la table `annonces`
 --
 ALTER TABLE `annonces`
-  MODIFY `id_ann` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id_ann` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT for table `mbr_admin`
+-- AUTO_INCREMENT pour la table `mbr_admin`
 --
 ALTER TABLE `mbr_admin`
   MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `mbr_free`
+-- AUTO_INCREMENT pour la table `mbr_free`
 --
 ALTER TABLE `mbr_free`
-  MODIFY `id_free` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id_free` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
--- AUTO_INCREMENT for table `mbr_society`
+-- AUTO_INCREMENT pour la table `mbr_society`
 --
 ALTER TABLE `mbr_society`
   MODIFY `id_soc` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `test`
+-- AUTO_INCREMENT pour la table `test`
 --
 ALTER TABLE `test`
   MODIFY `id_test` int(11) NOT NULL AUTO_INCREMENT;
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables exportées
 --
 
 --
--- Constraints for table `test`
+-- Contraintes pour la table `test`
 --
 ALTER TABLE `test`
   ADD CONSTRAINT `test_ibfk_1` FOREIGN KEY (`id_free`) REFERENCES `mbr_free` (`id_free`);
